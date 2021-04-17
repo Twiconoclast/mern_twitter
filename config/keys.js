c9032d0a832e739c50304f4029667645dfe771fc
Carol Bares <caroltbares@gmail.com>
2021-04-16 18:40:10 -0700

    set up project

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://DevUser:SAhBG8llI1rj4bIj@cluster0.rg8ll.mongodb.net/test?retryWrites=true&w=majority'
+            //Make sure this is your own unique string
+}
