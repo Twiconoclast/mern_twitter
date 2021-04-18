33092cee306154147107683f694ef5b3808d6a62
Carol Bares <72429572+Twiconoclast@users.noreply.github.com>
2021-04-17 19:46:42 -0700

    removing keys.js

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://DevUser:SAhBG8llI1rj4bIj@cluster0.rg8ll.mongodb.net/test?retryWrites=true&w=majority'
-            //Make sure this is your own unique string
-}
