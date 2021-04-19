9713d258ec43b9605e00ad979a1370685af66a1c
Carol Bares <caroltbares@gmail.com>
2021-04-19 02:08:15 -0700

    changed Port to PORT

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 mongoose
 .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
